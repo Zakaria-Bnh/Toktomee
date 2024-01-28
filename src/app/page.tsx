9db5cfd66846bac27202ede901821eb984f3d1e5
Zakaria-Bnh <zakariabenhanna2002@gmail.com>
2024-01-28 22:18:30 +0100

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main className='min-h-100vh'>
+    <h1 className='text-2xl grid place-items-center '>hello</h1>
+   </main>
+  );
+}
